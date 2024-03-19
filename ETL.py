11fcbb1362f3e589aa46029ae865ee6605fef6cd
Denis Shafarenko <phisika001@mail.ru>
2024-03-19 11:13:36 +0300

    DS-1.1 ETL
     Changes:
        - Created ETL pipeline, extracting data from csv files, applying transformations and loading to csv files
        - Added to transformation:
            - dropping negative values in `item_cnt_day` and `item_price`
            - converting `item_cnt_day` to int
            - dropping outliers in `item_price` and `item_cnt_day`
            - normalization

---
+++ ETL.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class ETL(ABC):
+
+    def __init__(self):
+        self.data = {}
+
+    def extract(self, path, name=None):
+        if name is None:
+            name = path
+
+        self.data[name] = pd.read_csv(path)
+
+    @abstractmethod
+    def transform(self):
+        pass
+
+    @abstractmethod
+    def load(self):
+        pass
